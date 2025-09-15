e126f29a150b91cae459d36c8c052e81068dd0ee
Zahid hasan <zmzahidhasan181@gmail.com>
2025-09-15 19:22:14 +0600

    update link

--- src/Page.jsx
+++ src/Page.jsx
@@ -9,11 +9,18 @@ import VideoCard from "./component/VideoCard";
 //   "https://www.youtube.com/embed/5g2tj2oe-ao"
 // ];
 const videos = [
-  "https://www.youtube.com/embed/8-ioO6m01o", 
+  "https://www.youtube.com/embed/yddqaxf-y2w", 
   "https://www.youtube.com/embed/42n8bHKOP94",
-  "https://www.youtube.com/watch?v=P6zpO979izs",
-  "https://www.youtube.com/embed/KTmPhev-0cg",
-  "https://www.youtube.com/embed/5g2tj2oe-ao"
+  "https://www.youtube.com/embed/P6zpO979izs",
+  "https://www.youtube.com/embed/2htheguVn98",
+  "https://www.youtube.com/embed/exODUQW1kkQ",
+  "https://www.youtube.com/embed/a7TOW_zYCfc",
+  "https://www.youtube.com/embed/FXYAnsUbqO8",
+  "https://www.youtube.com/embed/HqQaKbo28Ak",
+  "https://www.youtube.com/embed/GwPWBoceVp0",
+  "https://www.youtube.com/embed/M8E3WtyoDBY",
+  "https://www.youtube.com/embed/WryCx0sGBjc",
+  "https://www.youtube.com/embed/6pcAUOTZ-Fw",
 ];
 
 const VideoList = () => {
